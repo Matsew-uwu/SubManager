ea9a188b5be59852b41bcd6ec57a553513bd967a
Matthieu Randriantsoa <randriantsoamatthieu@gmail.com>
2022-02-20 00:59:31 +0100

    Creation de la structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Integration de Bootsrap5 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>SubManager - Home</title>
+</head>
+<body>
+    <?php include("./templates/header.php"); ?>
+    <div class="container-lg">
+        <h1 class="text-center py-3">Bienvenue sur SubManager, la plateforme de gestion des abonnees</h1>
+        <div class="text-center">
+            <a href="register.php" class="btn btn-dark">Se connecter</a>
+            <a href="login.php" class="btn btn-dark">S'inscrire</a>
+        </div>
+    </div>
+</body>
+</html>
